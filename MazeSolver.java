0b178c96cbf5cc152432ae2230055f83c4a416f5
GraceFeeley <gfeeley@stuy.edu>
2018-04-11 20:41:21 -0400

    Added solveMaze and populated the class

--- MazeSolver.java
+++ MazeSolver.java
@@ -7,6 +7,54 @@
  */
 
 
-public class Maze {
+public class MazeSolver {
+     ArrayList< Maze> solutions;
+     Maze inProgress;
+
+    public MazeSolver(String sourcefilename, int explorerRank, int explorerFile) {
+            
+        inProgress = new Maze(String sourcefilename, int explorerRank, int explorerFile);
+        solveMaze(); 
+    }
+
+public boolean solveMaze() {
+     //Explorer starts on a point
+
+     //Check base cases
+    if (inProgress.explorerIsOnA().equals("WALL");
+	return false;
+
+	//If on treasure, return true -- don't know how to do yet
+
+  
+
+	//Else, on stepping stone
+
+	//For every legal neighbor of explorer
+
+	int[] directions = new int{1,2,4.8};
+	
+
+	for(int a: directions){
+	    
+	    // Replace original stepping stone with a wall
+	    Maze backUp = new Maze(inProgress);
+
+	    inProgress.dropA(2);
+
+	    //Invoke recursive abstraction
+	    //When I am asked to solve a maze, given a maze, the recursive abstraction can solve a maze with one fewer stepping stones by making the next legal move.
+	    if(inProgress.go(a) == null) break;
+	    else inProgress.go(a);
+	    solveMaze;
+	    //Recover original stepping stone
+
+	    backUp.dropA(2);
+	}
+	
+	    
+	return false;
+	
+    }
 
 }
